14431b12c53b84491aaf03d2fba38cacc82840a4
Ben Partch <ben@retronetro.com>
2019-08-03 22:38:00 -0500

    init

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About BCav's coffee company" />
+    <h1>About</h1>
+    <p>Welcome to our about page. Check back later to actually learn about us! <span role="img" aria-label="cup of coffee">☕️</span></p>
+  </Layout>
+)
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title="About BCav's coffee company" />
+    <h1>Contact</h1>
+    <p>Send us an email: <a href="mailto:bcavscoffeeco@gmail.com">bcavscoffeeco@gmail.com</a> <span role="img" aria-label="cup of coffee">☕️</span></p>
+  </Layout>
+)
+
+export default ContactPage
