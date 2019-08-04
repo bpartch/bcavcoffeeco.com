ae249d772c1f8597651bfc03593a1beb2873c857
Ben Partch <ben@retronetro.com>
2019-08-03 22:45:38 -0500

    init

--- src/components/header.js
+++ src/components/header.js
@@ -76,9 +76,10 @@ const Header = ({ siteTitle }) => (
   <SiteHeader>
     <Content>
       <p>
-        <HomeLink to="/">{siteTitle}</HomeLink>
+        <HomeLink to="/">Home</HomeLink>
+        <NavLink to="/about">About</NavLink>
         <NavLink to="/blog">Blog</NavLink>
-        <GitHubLink href="https://github.com/niklasmtj/gatsby-starter-julia">GitHub</GitHubLink>
+        <NavLink to="/contact">Contact</NavLink>
       </p>
     </Content>
   </SiteHeader>
